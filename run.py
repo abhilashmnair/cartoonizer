9b8dc8c060bdd0e4084ed879b04683bcc4705756
Abhilash M Nair <abhilashmnair20@gmail.com>
2021-01-03 10:07:42 +0530

    initial Commit

---
+++ run.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+def readImage(path):
+    img = cv2.imread(path,1)
+    return img
+
+def edgeMask(img, lineSize, blurValue):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    grayBlur = cv2.medianBlur(gray, blurValue)
+    edges = cv2.adaptiveThreshold(grayBlur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, lineSize, blurValue)
+    return edges
+
+def colorQuantization(img, k):
+  data = np.float32(img).reshape((-1, 3))
+  criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
+  ret, label, center = cv2.kmeans(data, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+  center = np.uint8(center)
+  result = center[label.flatten()]
+  result = result.reshape(img.shape)
+  return result
+
+image = readImage('images/sample.jpg')
+edges = edgeMask(image, 7,7)
+image = colorQuantization(image, 9)
+blurred = cv2.bilateralFilter(image, 7, 200, 200)
+cartoon = cv2.bitwise_and(blurred, blurred, mask=edges)
+cv2.imwrite('images/processed.jpg',cartoon)
